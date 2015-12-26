7e1a871dc8532b94fcb638afc0a2d6302a97c8df
tundraka <tundraka@gmail.com>
2015-12-26 00:44:54 -0600

    routes.

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 var http = require('http');
 var fs = require('fs');
 
+function serveStaticFile(res, path, contentType, responseCode) {
+    if (!responseCode) {
+        responseCode = 200;
+    }
+
+    fs.readFile(__dirname + path, function(err, data) {
+        if (err) {
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('500: Internal server error');
+            return;
+        }
+
+        res.writeHead(responseCode, {'Content-Type': contentType});
+        res.end(data);
+    });
+}
+
 http.createServer(function(req, res) {
     var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
 
@@ -8,16 +25,16 @@ http.createServer(function(req, res) {
 
     switch (path) {
         case '': 
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('Hello world!');
+            serveStaticFile(res, '/public/home.html', 'text/html', 200);
             break;
         case '/about': 
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('About');
+            serveStaticFile(res, '/public/about.html', 'text/html', 200);
+            break;
+        case '/img/queenii.jpg': 
+            serveStaticFile(res, '/public/img/queenII.jpg', 'image/jpg', 200);
             break;
         default:
-            res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.end('Not found');
+            serveStaticFile(res, '/public/404.html', 'text/html', 404);
             break;
     }
 }).listen(3000);
