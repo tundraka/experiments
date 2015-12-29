949223d869cbfddeb86f05e53d646cce29b7654a
tundraka <tundraka@gmail.com>
2015-12-29 08:59:18 -0600

    Adding the tours pages for cross-page testing.

--- medowlark.js
+++ medowlark.js
@@ -41,6 +41,14 @@ app.get('/about', function(req, res) {
     });
 });
 
+app.get('/tours/hood-river', function(req, res) {
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res) {
+    res.render('tours/request-group-rate');
+});
+
 // 404
 app.use(function(req, res, next) {
     res.status(404);
